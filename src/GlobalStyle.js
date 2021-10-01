191fcb867751668812d27c03ee4bd6050b5f29bb
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-01 18:48:50 +0300

    refactor: GlobalStyle.js revised.

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,18 +1,18 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     :root {
         --maxWidth:1280px;
         --white:#fff;
         --lightGrey: #eee;
-        ---medGrey: #353535;
-        ---darkGrey: #1c1c1c;
+        --medGrey: #353535;
+        --darkGrey: #1c1c1c;
         --fontSuperBig: 2.5rem
         --fontBig: 1.5rem
         --fontMed:: 1.2rem
         --fontSmall: 1rem
-
     }
+
     *{
         box-sizing: border-box;
         font-family : 'Abel',sans-serif;
@@ -25,18 +25,17 @@ export const GlobalStyle = createGlobalStyle`
         h1{
             font-size: 2rem;
             font-weight: 600;
-           
+            color: var(--white)
         }
 
-        h2{
+        h3{
             font-size: 1.1rem;
             font-weight: 600;
         }
 
         p{
-            font-size: 1.1rem;
-            color: var(---white)
+            font-size: 1rem;
+            color: var(--white)
         }
     }
-
 `; 

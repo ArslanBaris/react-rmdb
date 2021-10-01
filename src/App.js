19dda86c4e94111b1c13c0a5b7ea4c46c7c57839
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-01 18:49:08 +0300

    refactor: App.js revised.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+// Components
+import Header from './components/Header';
+import Home from './components/Home'
 
 //Styles
 import { GlobalStyle } from './GlobalStyle';
@@ -7,7 +10,8 @@ import { GlobalStyle } from './GlobalStyle';
 function App() {
   return (
     <div className="App">
-      Start here.
+      <Header />
+      <Home />
       <GlobalStyle />
     </div>
   );

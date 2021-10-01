73e80ecba616e3f7932a49482462d90e4a6bea59
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-01 22:30:50 +0300

    refactor: Home.js revised.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,32 @@
 import React, {useState, useEffect} from "react";
-// API
-import API from '../API'
+
 // Config
 import {POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
+
 // Components
+import HeroImage from './HeroImage';
 
 // Hook
+import { useHomeFetch } from '../hooks/useHomeFetch';
 
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = '') => {
-    try {
-      setError(false);
-      setLoading(true);
-
-      const movies = await API.fetchMovies(searchTerm, page);
-
-      setState(prev => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-      }));
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(false);
+    const { state, loading, error } = useHomeFetch();
+  
+    console.log(state);
+  
+    return (
+      <>
+        {state.results[0] ? (
+          <HeroImage
+            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+            title={state.results[0].original_title}
+            text={state.results[0].overview}
+          />
+        ) : null}
+      </>
+    );
   };
-
-  // Initial render
-  useEffect(() => {
-    fetchMovies(1);
-  }, []);
-
-  console.log(state);
-
-  return <div>Home Page</div>;
-};
-
 export default Home;
